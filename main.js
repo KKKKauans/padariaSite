b084b49c61af99c0f9de73b71b7b3adb925aa52e
KKKKauans <132458589+KKKKauans@users.noreply.github.com>
2025-06-29 23:13:35 -0300

    Subindo o projeto

---
+++ main.js
@@ -0,0 +1,28 @@
+new Swiper(".carrossel-wrapper", {
+  loop: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //Responsividade para as telas
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
